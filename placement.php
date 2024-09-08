871f43fb43e1007b626a88b629f49cec7d44294b
savchuckvadim <savchuckvadim@gmail.com>
2024-09-08 18:22:32 +0300

    add finish

---
+++ placement.php
@@ -0,0 +1,7 @@
+
+<?php
+
+require_once(__DIR__ . '/crest.php');
+?>;
+
+<!doctype html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>April CRM</title><script defer="defer" src="main.e335ea793a2d861f8d65.js"></script><link href="css/main.758cb174.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
